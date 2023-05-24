f3e16e7ec118f8166c01139d60c806dc7da669a9
Tino Muzambi <tino.muzambi@uct.ac.za>
2023-05-24 14:25:27 +0200

    Adds styles.

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -5,6 +5,20 @@ type TodoItemProps = {
 };
 
 const TodoItem = ({ id, title, complete }: TodoItemProps) => {
-	return <li key={id}>{title}</li>;
+	return (
+		<li
+			className="flex gap-1
+     items-center"
+			key={id}
+		>
+			<input type="checkbox" id={id} className="cursor-pointer peer" />
+			<label
+				htmlFor={id}
+				className={`cursor-pointer peer-checked:line-through peer-checked:text-slate-500`}
+			>
+				{title}
+			</label>
+		</li>
+	);
 };
 export default TodoItem;
